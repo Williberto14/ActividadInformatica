807d9044d45d56c1d3218fd0c6d588e15b1d3f40
henrysalo <hessuarezlo@unal.edu.co>
2021-03-26 16:36:57 -0500

    ambos cambios se quedan y se queda

--- negro.js
+++ negro.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //el día comenzó frío
 
 function haceCalor(temperatura){
@@ -8,7 +7,5 @@ function haceCalor(temperatura){
         console.log("Vaya por la sombita");
     }
 }
-=======
 // el día comenzó frío y el chocolate no estaba disponible 
 console.log("solecito en forma")
->>>>>>> 3a78cb5862b6297e53802d709fcb5f4a58c9e114
