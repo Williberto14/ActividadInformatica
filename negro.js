37c3ec0ba455f1648fd034b43378deae3972d526
Williberto14 <williberto14@gmail.com>
2021-03-26 16:29:11 -0500

    Modificando el mismo archivo

--- negro.js
+++ negro.js
@@ -1,1 +1,9 @@
 //el día comenzó frío
+
+function haceCalor(temperatura){
+    if (temperatura < 30){
+        console.log("no hace calor");
+    }else{
+        console.log("Vaya por la sombita");
+    }
+}
